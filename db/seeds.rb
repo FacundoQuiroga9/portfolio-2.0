ec3e1d3ca8796fe58f30e312bab56189bd163e2b
Facundo Quiroga <facu.quirogaga@gmail.com>
2024-06-03 13:44:13 -0500

    Intentado arreglar el bug de la imagen del proyecto olsc

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,7 +13,7 @@ Project.create!(
   technology: ror,
   github_url: 'https://github.com/elisaherre/FIXI_SERVICE',
   website_url: 'http://www.fixiservice.xyz/',
-  image: { io: URI.open('https://res.cloudinary.com/dhobfoyaf/image/upload/v1717438409/wshenruc3hlrp1p4ptfv4uzfx4uz.jpg'), filename: 'your_image.jpg' }
+  image: { io: URI.open('https://res.cloudinary.com/dhobfoyaf/image/upload/v1717438409/wshenruc3hlrp1p4ptfv4uzfx4uz.jpg'), filename: 'fixi.jpg' }
 )
 
 Project.create!(
@@ -22,7 +22,7 @@ Project.create!(
   technology: ror,
   github_url: 'https://github.com/FacundoQuiroga9/blog',
   website_url: '',
-  image: { io: URI.open('https://res.cloudinary.com/dhobfoyaf/image/upload/v1717437913/heir7d3357miye0bh3ie364s8wtj.png'), filename: 'your_image.jpg' }
+  image: { io: URI.open('https://res.cloudinary.com/dhobfoyaf/image/upload/v1717439908/27kncuu3n27th9x4xk2pmypysl3z.png'), filename: 'olsc_bsas.jpg' }
 )
 
 Project.create!(
@@ -31,7 +31,7 @@ Project.create!(
   technology: react,
   github_url: 'https://github.com/FacundoQuiroga9/Ffarm-Taid',
   website_url: 'https://hosteriaffarmtaid.com/',
-  image: { io: URI.open('https://res.cloudinary.com/dhobfoyaf/image/upload/v1717438424/5nn1twiokb1123p989j3xmgjl7uc.jpg'), filename: 'your_image.jpg' }
+  image: { io: URI.open('https://res.cloudinary.com/dhobfoyaf/image/upload/v1717438424/5nn1twiokb1123p989j3xmgjl7uc.jpg'), filename: 'ffarm_taid.jpg' }
 )
 
 Project.create!(
@@ -40,5 +40,5 @@ Project.create!(
   technology: react,
   github_url: 'https://github.com/FacundoQuiroga9/justice-fireworks',
   website_url: 'https://dynamitefireworks.uno/',
-  image: { io: URI.open('https://res.cloudinary.com/dhobfoyaf/image/upload/v1717438435/yqxi2h9uhwife092mhrwm64dgmzr.jpg'), filename: 'your_image.jpg' }
+  image: { io: URI.open('https://res.cloudinary.com/dhobfoyaf/image/upload/v1717438435/yqxi2h9uhwife092mhrwm64dgmzr.jpg'), filename: 'dynamite_fireworks.jpg' }
 )
